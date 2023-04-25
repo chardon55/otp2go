7cf5fe28a27c21a0cd69590c0c0893515597933b
Charles Dong <charlesdong_2000@outlook.com>
2023-04-25 07:38:10 +0000

    Update byteorder utility

--- byteorder/byteorder.go
+++ byteorder/byteorder.go
@@ -9,7 +9,7 @@ func ByteOrder() binary.ByteOrder {
 	buffer := [2]byte{}
 	*(*uint16)(unsafe.Pointer(&buffer[0])) = uint16(0x00FF)
 
-	if buffer == [2]byte{0xFF, 0x00} {
+	if buffer[0] == 0xFF {
 		return binary.LittleEndian
 	}
 
